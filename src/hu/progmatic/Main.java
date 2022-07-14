6a9969d2cd20474a15ca847c2b8a3f1ab6243f51
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-07-14 17:26:49 +0200

    bm: Quiz class

--- src/hu/progmatic/Main.java
+++ src/hu/progmatic/Main.java
@@ -5,7 +5,10 @@ import hu.progmatic.model.Quiz;
 public class Main {
     public static void main(String[] args) {
         Quiz[] quizzes = {
-
+                new Quiz("2022.07.12.", "Enter Bar", 120, 40, 7, 28, 3000),
+                new Quiz("2022.07.19.", "Enter Bar", 110, 50, 6, 36, 5000),
+                new Quiz("2022.07.12.", "Rácskert", 100, 42, 4, 20, 10000),
+                new Quiz("2022.07.19.", "Rácskert", 150, 52, 8, 40, 10000)
         };
     }
 }
